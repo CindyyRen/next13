8ff4b9895ab0e71b2bcf2054d5ac4cf787557e4f
Cindy Ren <bdcindy78@gmail.com>
2023-11-06 18:24:58 +1100

    add filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,6 +3,9 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import LocalSearchBar from '../../../components/shared/search/LocalSearchBar';
+import Filter from '@/components/shared/Filter';
+import { HomePageFilters } from '@/constants/filters';
+import HomeFilters from '@/components/home/HomeFilters';
 
 export default function Home() {
   return (
@@ -23,7 +26,13 @@ export default function Home() {
           placeholder="Search for questions"
           otherClasses="flex-1"
         />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+      <HomeFilters />
     </>
   );
 }
